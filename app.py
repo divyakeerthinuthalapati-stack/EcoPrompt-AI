617b80c734bb947e84da074d786cf10f32f8001e
divyakeerthinuthalapati-stack <divyakeerthinuthalapati@gmail.com>
2026-07-22 22:34:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,10 +39,7 @@ def generate():
 
     except Exception as e:
         print("ERROR:", e)
-        return jsonify({
-            "error": str(e)
-        }), 500
-
+        
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
